a4ecb9137b5c7e5d877578ee8f75083682a8796c
King Khan <nsikan.na@gmail.com>
2022-01-26 15:27:29 -0500

    fixed tailwind ccs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
-=======
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
@@ -20,4 +7,3 @@ module.exports = {
   },
   plugins: [],
 }
->>>>>>> bbdbaf3060a1b66e1d1128c4c385d9c33d266c56
