a201d81600a884bae3366825140b54ece291811d
King Khan <nsikan.na@gmail.com>
2022-01-26 23:48:06 -0500

    fixed links

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default function App() {
             desc="This is a application where a teacher can manage their student's information. The teacher is able to view all the information without signing in, but must sign in to be able to add, update, or delete student information. The teacher can setup a new account if they do not already have one, and can also change their password once they sign in."
             other="(Fake Login Provided)"
             skills={["html", "css", "js", "php", "sql"]}
-            git="https://github.com/nsikan-na/portfolio"
+            git="https://github.com/nsikan-na/student-info"
             live="https://nakpan.info/studentinfo/intro.php"
           />
           <br />
