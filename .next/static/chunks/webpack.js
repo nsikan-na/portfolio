114afbb624645e442d3f33cefdb868f4c88438c2
King Khan <nsikan.na@gmail.com>
2022-03-23 22:11:00 -0400

    fixed nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c440131fb2874402"; }
+/******/ 		__webpack_require__.h = function() { return "5f5da57b35816153"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
