c69006f3dbc8b55a9951ba5f1d51d7268d32f32d
King Khan <nsikan.na@gmail.com>
2022-04-18 20:24:22 -0400

    inital color changes every refresh

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,19 @@ import changeColor from "../util/changeColor";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const [alert, setAlert] = useState<{}>({});
-
+  const themeColor: string[] = [
+    //array of colors
+    "#00CCFF",
+    "#FF0000",
+    "green",
+    "#FF00D0",
+    "#AE00FF",
+    "#0033FF",
+    "#FF8800",
+  ];
   useEffect(() => {
-    changeColor("#00BFFF");
+    //sets the initial theme color
+    changeColor(themeColor[Math.floor(Math.random() * themeColor.length)]);
   }, []);
   useEffect(() => {
     //AOS is for the animation
