0d12c56ad343eab7d02e504b47882d854ec8b6ae
King Khan <nsikan.na@gmail.com>
2022-05-09 22:39:55 -0400

    changed graduation text

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -7,10 +7,7 @@ export default function Skills() {
     <div className="primaryBackground sectionPadding ">
       <Header text="Skills" textColor="white" />
       <div className="md:flex justify-evenly ">
-        <div
-          className="flex flex-wrap justify-evenly xl:grid xl:grid-cols-3 xl:gap-2 md:w-5/12 lg:w-4/12"
-    
-        >
+        <div className="flex flex-wrap justify-evenly xl:grid xl:grid-cols-3 xl:gap-2 md:w-5/12 lg:w-4/12">
           {[
             "Html",
             "Css",
@@ -37,29 +34,24 @@ export default function Skills() {
             </div>
           ))}
         </div>
-        <div
-          className="mt-4 md:w-5/12 text-white font-semibold lg:w-4/12 text-xl"
-         
-        >
+        <div className="mt-4 md:w-5/12 text-white font-semibold lg:w-4/12 text-xl">
           <p className="indent-12">
             I am software developer passionate about developing interactive,
             user-friendly websites. I am proficient in Html, Css, Bootstrap,
             Tailwind.css, Javascript, Typescript, React.js, Next.js, Node.js,
             Git/GitHub, SQL, NoSQL, and MongoDB, but most importantly, I am
-            confident in my ability to learn and adapt to what my team
-            needs.
+            confident in my ability to learn and adapt to what my team needs.
           </p>
           <br />
           <p className="indent-12	">
-            I will graduate from{" "}
+            {`I graduated from Gwinnett Technical College with an associates degree in `}
             <a
               href="https://gwinnetttech.edu/websitedesignanddevelopment/"
               target="_blank"
               className=" underline hover:text-gray-300"
             >
-              Gwinnett Technical College
-            </a>{" "}
-            with a degree in Web Design and Development in May 2022.
+              {`Web Design and Development`}
+            </a>{`.`}
           </p>
         </div>
       </div>
