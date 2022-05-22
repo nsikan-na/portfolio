68d5c473400a0aef23589bbd5de2d35fd5c3876a
King Khan <nsikan.na@gmail.com>
2022-05-21 22:35:44 -0400

    altered gd desc

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,6 +8,32 @@ export default function Projects() {
       <Header text="Projects" />
       <div className="text-center primaryText text-xl -mt-3">{`(Click Image to Play/Pause Demo Gif)`}</div>
       <ProjectTemplate
+        title="Gwinnett Diner"
+        skills={[
+          "Tailwind css",
+          "Bootstrap",
+          "Typescript",
+          "React.js",
+          "Next.js",
+          "Context Api",
+          "Bcrypt",
+          "NoSql",
+          "MongoDB",
+        ]}
+        desc={[
+          `Responsive interface`,
+          `Manage state using React's context api`,
+          `Validate user input`,
+          `Encrypt sensitive data using bcrypt`,
+          `Capstone Project for my Associate Degree`,
+          `Store user data in database using NoSql`,
+          `Work on a team of 8 (I was a developer)`,
+        ]}
+        githubLink="https://github.com/nsikan-na/Gwinnett_Dinner-v2"
+        demoLink="https://gwinnett-diner-v2.vercel.app/"
+        gif="diner"
+      />{" "}
+      <ProjectTemplate
         title="CryptoTracker"
         skills={[
           "Next.js",
@@ -50,31 +76,6 @@ export default function Projects() {
         githubLink="https://github.com/nsikan-na/investment_calulator"
         demoLink="https://ramsey-investment-calculator.vercel.app/"
         gif="investment"
-      />
-      <ProjectTemplate
-        title="Gwinnett Diner"
-        skills={[
-          "Tailwind css",
-          "Bootstrap",
-          "Typescript",
-          "React.js",
-          "Next.js",
-          "Context Api",
-          "Bcrypt",
-          "NoSql",
-          "MongoDB",
-        ]}
-        desc={[
-          `Responsive interface`,
-          `Manage state using React's context api`,
-          `Validate user input`,
-          `Encrypt sensitive data using bcrypt`,
-          `Store user data in database using NoSql`,
-          `Work on a team (I was a developer)`,
-        ]}
-        githubLink="https://github.com/nsikan-na/Gwinnett_Dinner-v2"
-        demoLink="https://gwinnett-diner-v2.vercel.app/"
-        gif="diner"
       />
     </div>
   );
