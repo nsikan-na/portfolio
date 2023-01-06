1f64a7978b392a48d093be5ec2004d9d2e1a54da
Nsikan Akpan <nsikan.akpan@betmakers.com>
2023-01-06 17:11:19 -0500

    added smv calc project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -59,6 +59,15 @@ export default function Projects() {
         demoLink="https://gwinnett-diner-v2.vercel.app/"
         gif="diner"
       />
+      <ProjectTemplate
+        title="SMV Calculator (Under Construction)"
+        skills={["React js", "Typescript"]}
+        desc={`The application is used to calculates your sexual market value as a man.`}
+        githubLink="https://github.com/nsikan-na/smvCalc"
+        demoLink="https://smv-calc.vercel.app/"
+        gif="smv"
+        gifNotReady={true}
+      />
     </div>
   );
 }
