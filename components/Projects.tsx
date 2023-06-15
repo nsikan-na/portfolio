368a4e3da00dce50638aa4958641cf010462d06b
Nsikan Akpan <nsikan.akpan@betmakers.com>
2023-06-15 01:30:18 -0400

    improved resume

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,7 +20,8 @@ export default function Projects() {
           "MongoDB",
           "Typescript",
         ]}
-        desc={`This is an application that takes displays useful information about the top crypto coins. It also includes many common features that add to the user experience.`}
+        desc={` Displays useful information about the top crypto coins.
+        `}
         githubLink="https://github.com/nsikan-na/CryptoTracker"
         demoLink="https://cryptocoins-tracker.vercel.app/"
         gif="crypto"
@@ -28,15 +29,16 @@ export default function Projects() {
       <ProjectTemplate
         title="Charlie Glass"
         skills={["Material Ui", "React.js", "Next.js", "Typescript"]}
-        desc={`This is a business website I made for a company name Charlie Glass.`}
+        desc={`A website for a company named Charlie Glass.
+        `}
         demoLink="https://charlieglassinc.com/"
         gif="charlie"
       />
       <ProjectTemplate
         title="Ramsey Investment Calculator"
         skills={["Tailwind css", "React js", "Next js", "Typescript"]}
-        desc={`This is an application that calculates the estimated retirement savings of an individual based on user input. A chart
-  is also generated to demonstrate when the user becomes a millionaire.`}
+        desc={`Calculates the estimated retirement savings of an individual based on user input. A chart is also generated to demonstrate when the user becomes a millionaire.
+        `}
         githubLink="https://github.com/nsikan-na/investment_calulator"
         demoLink="https://ramsey-investment-calculator.vercel.app/"
         gif="investment"
@@ -52,9 +54,7 @@ export default function Projects() {
           "MongoDB",
           "Typescript",
         ]}
-        desc={`This was the capstone project of my associate's degree at Gwinnett Technical College. It is an eCommerce
-        site for a fictitious restaurant name Gwinnett Diner. I worked on a team of 8 students as a developer to complete this project. The project was
-        made up of 4 sprints, so we met regularly to share how our portion of the application was developing.`}
+        desc={`An eCommerce site for a fictitious restaurant named Gwinnett Diner. I worked on a team of 8 students as a developer to complete this project. The project was made up of 4 sprints, so we met regularly to share how our portion of the application was developing. The capstone project of my associate degree at Gwinnett Technical College.`}
         githubLink="https://github.com/nsikan-na/Gwinnett_Dinner-v2"
         demoLink="https://gwinnett-diner-v2.vercel.app/"
         gif="diner"
