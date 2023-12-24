3c5c6158c5c2fb00d50a0081e30f242f1eb42420
nsikan.akpan <nsikan.akpan@betmakers.com>
2023-12-24 02:37:33 -0500

    changed skills

--- pages/components/Skills.tsx
+++ pages/components/Skills.tsx
@@ -11,34 +11,27 @@ export default function Skills() {
       <Header text="Skills" textColor="white" />
       <div className="md:flex justify-evenly items-center">
         <div className="flex flex-wrap justify-evenly md:w-5/12 lg:w-4/12">
-          {[
-            "Html",
-            "Css",
-            "Javascript",
-            "React.js",
-            "Typescript",
-            "Next.js",
-            "Git",
-          ].map((skill: string) => (
-            <div key={skill}>
-              <Image
-                src={`/images/newskills/${skill}.png`}
-                alt={`${skill} icon`}
-                title={skill}
-                width="75%"
-                height="75%"
-                className="cursor-auto "
-              />
-            </div>
-          ))}
+          {["Html", "Css", "Javascript", "React.js", "Typescript", "Git", "Sql"].map(
+            (skill: string) => (
+              <div key={skill}>
+                <Image
+                  src={`/images/newskills/${skill}.png`}
+                  alt={`${skill} icon`}
+                  title={skill}
+                  width="75%"
+                  height="75%"
+                  className="cursor-auto "
+                />
+              </div>
+            )
+          )}
         </div>
         <div className="mt-4 md:w-5/12 text-white font-semibold lg:w-4/12 text-xl">
           <p className="indent-12">
             I am a software developer focusing on web development. Technologies
-            I use include Html, Css, Javascript, Typescript, React, Redux,
-            Node.js, Next.js, Docker, Git, Java, Sql, NoSql, MongoDB, Postgres,
-            Photoshop, Tailwind.css, and various Css Frameworks. Most
-            importantly, I can adapt to the needs of my team.
+            I use include Html, Css, Javascript, Typescript, React.js, Node.js,
+            Docker, Git, Java, and Sql. Most importantly, I can adapt to the
+            needs of my team.
           </p>
         </div>
       </div>
