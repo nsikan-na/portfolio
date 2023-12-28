b445a08b9b1f8d4f18aea7aa9e98174adb7a6968
Nsikan Akpan <nsikan.akpan@betmakers.com>
2023-12-28 00:28:50 -0500

    changed skills

--- pages/components/Skills.tsx
+++ pages/components/Skills.tsx
@@ -11,27 +11,34 @@ export default function Skills() {
       <Header text="Skills" textColor="white" />
       <div className="md:flex justify-evenly items-center">
         <div className="flex flex-wrap justify-evenly md:w-5/12 lg:w-4/12">
-          {["Html", "Css", "Javascript", "React.js", "Typescript", "Git", "Sql"].map(
-            (skill: string) => (
-              <div key={skill}>
-                <Image
-                  src={`/images/newskills/${skill}.png`}
-                  alt={`${skill} icon`}
-                  title={skill}
-                  width="75%"
-                  height="75%"
-                  className="cursor-auto "
-                />
-              </div>
-            )
-          )}
+          {[
+            "Html",
+            "Css",
+            "Javascript",
+            "React.js",
+            "Typescript",
+            "Node.js",
+            "Git",
+            "Sql",
+          ].map((skill: string) => (
+            <div key={skill}>
+              <Image
+                src={`/images/newskills/${skill}.png`}
+                alt={`${skill} icon`}
+                title={skill}
+                width="75%"
+                height="75%"
+                className="cursor-auto "
+              />
+            </div>
+          ))}
         </div>
         <div className="mt-4 md:w-5/12 text-white font-semibold lg:w-4/12 text-xl">
           <p className="indent-12">
             I am a software developer focusing on web development. Technologies
-            I am focusing on include Html, Css, Javascript, Typescript, React.js, Node.js,
-            Docker, Git, Java, and Sql. Most importantly, I can adapt to the
-            needs of my team.
+            I am focusing on include Html, Css, Javascript, Tailwind.css, Typescript,
+            React.js, Express.js, Node.js, Docker, Git, Java, Spring Boot, and Sql. Most importantly, I
+            can adapt to the needs of my team.
           </p>
         </div>
       </div>
