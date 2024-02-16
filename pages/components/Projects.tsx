40b2865c4b54e0c45793aed5e07cdddcbf804e46
Nsikan Akpan <nsikan.akpan@betmakers.com>
2024-02-15 21:24:05 -0500

    changed cold starty

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProjectTemplate from "./ProjectTemplate";
 import Header from "./Header";
 
-export default function Projects({}) {
+export default function Projects() {
   return (
     <div
       className="sectionPadding"
@@ -74,40 +74,4 @@ export default function Projects({}) {
       />
     </div>
   );
-}
-
-export const getStaticProps: any = async () => {
-  sendAPIRequest();
-
-  return {
-    props: {},
-    revalidate: 300,
-  };
-};
-
-function sendAPIRequest() {
-  const urls = ["https://charlie-glass-admin-api.vercel.app/"];
-
-  const options = {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  urls.forEach((url) =>
-    fetch(url, options)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("There was a problem with your fetch operation:", error);
-      })
-  );
-  console.log("Ran Apis (cold start)");
 }
