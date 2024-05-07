62ec309dfcf27199b2814195ab373f767fdea2e1
Nsikan <94437730+nsikan-na@users.noreply.github.com>
2024-05-07 01:11:25 -0400

    Update Skills.tsx

--- pages/components/Skills.tsx
+++ pages/components/Skills.tsx
@@ -35,7 +35,7 @@ export default function Skills() {
         </div>
         <div className="mt-4 md:w-5/12 text-white font-semibold lg:w-4/12 text-xl">
           <p className="indent-12">
-           I am a software developer focusing on full-stack web development. Technologies I have experience with include React.js, Typescript, Node.js, Git, and Sql. Most importantly, I can adapt to the needs of my team. I am completing a Bachelor of Science degree in Computer Science for a stronger foundation in Software Development.
+           I am a software developer with 2+ years of experience focusing on full-stack web development. Technologies I have experience with include React.js, Typescript, Node.js, Git, and Sql. Most importantly, I can adapt to the needs of my team. I am completing a Bachelor of Science degree in Computer Science for a stronger foundation in Software Development.
           </p>
         </div>
       </div>
