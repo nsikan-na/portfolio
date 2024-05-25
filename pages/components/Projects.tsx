7109c02d974786cc88ac04a136b7573aeacfa5c5
Nsikan <94437730+nsikan-na@users.noreply.github.com>
2024-05-25 13:58:42 -0400

    Update Projects.tsx

--- pages/components/Projects.tsx
+++ pages/components/Projects.tsx
@@ -23,7 +23,7 @@ export default function Projects() {
         ]}
         desc={`My collaborator and I worked closely with the customer to design and develop a web application that allows a company to gain insights such as tracking profits in a dashboard. Users can also create, view, download, and print quotes and invoices.
         `}
-        githubLink="https://github.com/nsikan-na/charlie-glass-admin"
+        githubLink="https://github.com/nsikan-na/charlie-glass-admin-api"
         demoLink="https://charlie-glass-admin.vercel.app/login"
         gif="cgiadmin1"
       />
